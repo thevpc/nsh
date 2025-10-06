3db5fdad7b6d9b9ee90cf8358b2662a416fa2895
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:24:54 +0100

    Update DfCommand.java : bump nuts 0.8.7

--- nsh-lib/src/main/java/net/thevpc/nsh/cmd/impl/posix/DfCommand.java
+++ nsh-lib/src/main/java/net/thevpc/nsh/cmd/impl/posix/DfCommand.java
@@ -25,8 +25,8 @@
  */
 package net.thevpc.nsh.cmd.impl.posix;
 
-import net.thevpc.nuts.NConstants;
-import net.thevpc.nuts.NSession;
+import net.thevpc.nuts.core.NConstants;
+import net.thevpc.nuts.core.NSession;
 import net.thevpc.nuts.text.NTextFormat;
 import net.thevpc.nuts.cmdline.NArg;
 import net.thevpc.nuts.cmdline.NCmdLine;
