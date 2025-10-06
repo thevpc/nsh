61512b502d7c1c8b3feab234b61fed3c67438d33
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:24:54 +0100

    Update WhoamiCommand.java : bump nuts 0.8.7

--- nsh-lib/src/main/java/net/thevpc/nsh/cmd/impl/posix/WhoamiCommand.java
+++ nsh-lib/src/main/java/net/thevpc/nsh/cmd/impl/posix/WhoamiCommand.java
@@ -25,11 +25,16 @@
  */
 package net.thevpc.nsh.cmd.impl.posix;
 
-import net.thevpc.nuts.*;
 import net.thevpc.nuts.cmdline.NArg;
 import net.thevpc.nuts.cmdline.NCmdLine;
-import net.thevpc.nuts.NUser;
+import net.thevpc.nuts.core.NConstants;
+import net.thevpc.nuts.core.NSession;
+import net.thevpc.nuts.core.NWorkspace;
+import net.thevpc.nuts.io.NOut;
+import net.thevpc.nuts.core.NRepository;
+import net.thevpc.nuts.security.NUser;
 import net.thevpc.nuts.io.NPrintStream;
+import net.thevpc.nuts.security.NWorkspaceSecurityManager;
 import net.thevpc.nuts.spi.NComponentScope;
 import net.thevpc.nuts.spi.NScopeType;
 import net.thevpc.nuts.text.NTextStyle;
