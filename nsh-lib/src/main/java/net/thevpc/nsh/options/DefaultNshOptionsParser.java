6e527fcf710b4bd73ab966faeae7175a6e53836d
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:24:55 +0100

    Update DefaultNshOptionsParser.java : bump nuts 0.8.7

--- nsh-lib/src/main/java/net/thevpc/nsh/options/DefaultNshOptionsParser.java
+++ nsh-lib/src/main/java/net/thevpc/nsh/options/DefaultNshOptionsParser.java
@@ -1,6 +1,6 @@
 package net.thevpc.nsh.options;
 
-import net.thevpc.nuts.NSession;
+import net.thevpc.nuts.core.NSession;
 import net.thevpc.nuts.util.NMsg;
 import net.thevpc.nuts.cmdline.NCmdLine;
 import net.thevpc.nsh.err.NshException;
