e27f36ac8665ce6f0c456d5917582fbc00a1c673
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:24:54 +0100

    Update SshCommand.java : bump nuts 0.8.7

--- nsh-lib/src/main/java/net/thevpc/nsh/cmd/impl/posix/SshCommand.java
+++ nsh-lib/src/main/java/net/thevpc/nsh/cmd/impl/posix/SshCommand.java
@@ -25,9 +25,12 @@
  */
 package net.thevpc.nsh.cmd.impl.posix;
 
-import net.thevpc.nuts.*;
 import net.thevpc.nuts.cmdline.NArg;
 import net.thevpc.nuts.cmdline.NCmdLine;
+import net.thevpc.nuts.command.NSearchCmd;
+import net.thevpc.nuts.core.NConstants;
+import net.thevpc.nuts.core.NSession;
+import net.thevpc.nuts.io.NOut;
 import net.thevpc.nuts.io.NPath;
 import net.thevpc.nuts.ext.ssh.SShConnection;
 import net.thevpc.nsh.cmd.NshBuiltinDefault;
