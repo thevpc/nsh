089a6b79daf073a7169a2448c7b1f52a166320ca
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:49:42 +0100

    Update PropsCommand.java : refactor NMsg

--- nsh-lib/src/main/java/net/thevpc/nsh/cmd/impl/common/PropsCommand.java
+++ nsh-lib/src/main/java/net/thevpc/nsh/cmd/impl/common/PropsCommand.java
@@ -30,7 +30,6 @@ import net.thevpc.nuts.cmdline.NArgName;
 import net.thevpc.nuts.cmdline.NCmdLine;
 
 import net.thevpc.nuts.command.NExecutionException;
-import net.thevpc.nuts.core.NConstants;
 import net.thevpc.nuts.core.NSession;
 import net.thevpc.nuts.core.NWorkspace;
 import net.thevpc.nuts.text.NObjectFormat;
@@ -38,7 +37,7 @@ import net.thevpc.nuts.io.NPath;
 import net.thevpc.nsh.cmd.NshBuiltinDefault;
 import net.thevpc.nsh.eval.NshExecutionContext;
 import net.thevpc.nsh.util.ShellHelper;
-import net.thevpc.nuts.util.NMsg;
+import net.thevpc.nuts.text.NMsg;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -51,7 +50,7 @@ import java.util.*;
 public class PropsCommand extends NshBuiltinDefault {
 
     public PropsCommand() {
-        super("props", NConstants.Support.DEFAULT_SUPPORT,Options.class);
+        super("props", DEFAULT_SCORE,Options.class);
     }
 
     @Override
