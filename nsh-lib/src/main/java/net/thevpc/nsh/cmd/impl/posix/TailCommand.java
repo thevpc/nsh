e45236702b031ea6990b9f3a8ef590f2feecead7
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:49:43 +0100

    Update TailCommand.java : refactor NMsg

--- nsh-lib/src/main/java/net/thevpc/nsh/cmd/impl/posix/TailCommand.java
+++ nsh-lib/src/main/java/net/thevpc/nsh/cmd/impl/posix/TailCommand.java
@@ -28,13 +28,12 @@ package net.thevpc.nsh.cmd.impl.posix;
 import net.thevpc.nuts.cmdline.NArg;
 import net.thevpc.nuts.cmdline.NCmdLine;
 import net.thevpc.nuts.command.NExecutionException;
-import net.thevpc.nuts.core.NConstants;
 import net.thevpc.nuts.core.NSession;
 import net.thevpc.nuts.io.NPath;
 import net.thevpc.nsh.cmd.NshBuiltinDefault;
 import net.thevpc.nsh.eval.NshExecutionContext;
 import net.thevpc.nsh.util.ShellHelper;
-import net.thevpc.nuts.util.NMsg;
+import net.thevpc.nuts.text.NMsg;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -50,7 +49,7 @@ import java.util.List;
 public class TailCommand extends NshBuiltinDefault {
 
     public TailCommand() {
-        super("tail", NConstants.Support.DEFAULT_SUPPORT, Options.class);
+        super("tail", DEFAULT_SCORE, Options.class);
     }
 
     @Override
