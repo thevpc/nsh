a6b9def42675807f319a4c7d7e5cffbc8b95310e
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:49:42 +0100

    Update BaseNameCommand.java : refactor NMsg

--- nsh-lib/src/main/java/net/thevpc/nsh/cmd/impl/bash/BaseNameCommand.java
+++ nsh-lib/src/main/java/net/thevpc/nsh/cmd/impl/bash/BaseNameCommand.java
@@ -25,7 +25,6 @@
  */
 package net.thevpc.nsh.cmd.impl.bash;
 
-import net.thevpc.nuts.core.NConstants;
 import net.thevpc.nuts.io.NOut;
 import net.thevpc.nuts.cmdline.NArg;
 import net.thevpc.nuts.cmdline.NCmdLine;
@@ -45,7 +44,7 @@ import java.util.List;
 public class BaseNameCommand extends NshBuiltinDefault {
 
     public BaseNameCommand() {
-        super("basename", NConstants.Support.DEFAULT_SUPPORT, Options.class);
+        super("basename", DEFAULT_SCORE, Options.class);
     }
 
     @Override
