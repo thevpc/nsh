52d5db5b7552ec490b4fa2ed8ad3678cd45cdc60
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:49:42 +0100

    Update NshBuiltinCore.java : refactor NMsg

--- nsh-lib/src/main/java/net/thevpc/nsh/cmd/NshBuiltinCore.java
+++ nsh-lib/src/main/java/net/thevpc/nsh/cmd/NshBuiltinCore.java
@@ -3,16 +3,16 @@ package net.thevpc.nsh.cmd;
 import java.util.function.Supplier;
 
 public abstract class NshBuiltinCore extends NshBuiltinBase {
-    public NshBuiltinCore(String name, int supportLevel, Class<?> optionsSupplier) {
-        super(name, supportLevel, optionsSupplier);
+    public NshBuiltinCore(String name, int score, Class<?> optionsSupplier) {
+        super(name, score, optionsSupplier);
     }
 
     public NshBuiltinCore(String name, Class<?> optionsSupplier) {
         super(name, optionsSupplier);
     }
 
-    public NshBuiltinCore(String name, int supportLevel, Supplier<?> optionsSupplier) {
-        super(name, supportLevel, optionsSupplier);
+    public NshBuiltinCore(String name, int score, Supplier<?> optionsSupplier) {
+        super(name, score, optionsSupplier);
     }
 
     public NshBuiltinCore(String name, Supplier<?> optionsSupplier) {
