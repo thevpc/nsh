723acdf3b16bb536e16da59f95e67b0dd74d52da
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:49:42 +0100

    Update UnsetCommand.java : refactor NMsg

--- nsh-lib/src/main/java/net/thevpc/nsh/cmd/impl/bash/UnsetCommand.java
+++ nsh-lib/src/main/java/net/thevpc/nsh/cmd/impl/bash/UnsetCommand.java
@@ -25,7 +25,6 @@
  */
 package net.thevpc.nsh.cmd.impl.bash;
 
-import net.thevpc.nuts.core.NConstants;
 import net.thevpc.nuts.cmdline.NArg;
 import net.thevpc.nuts.cmdline.NCmdLine;
 import net.thevpc.nuts.core.NSession;
@@ -45,7 +44,7 @@ import java.util.Set;
 public class UnsetCommand extends NshBuiltinDefault {
 
     public UnsetCommand() {
-        super("unset", NConstants.Support.DEFAULT_SUPPORT, Options.class);
+        super("unset", DEFAULT_SCORE, Options.class);
     }
 
     @Override
