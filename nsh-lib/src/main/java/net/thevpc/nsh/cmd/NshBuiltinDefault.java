e0aca5caa5bdf77634aa90da795eb4c34738427b
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:49:42 +0100

    Update NshBuiltinDefault.java : refactor NMsg

--- nsh-lib/src/main/java/net/thevpc/nsh/cmd/NshBuiltinDefault.java
+++ nsh-lib/src/main/java/net/thevpc/nsh/cmd/NshBuiltinDefault.java
@@ -3,16 +3,16 @@ package net.thevpc.nsh.cmd;
 import java.util.function.Supplier;
 
 public abstract class NshBuiltinDefault extends NshBuiltinBase {
-    public NshBuiltinDefault(String name, int supportLevel, Class<?> optionsSupplier) {
-        super(name, supportLevel, optionsSupplier);
+    public NshBuiltinDefault(String name, int score, Class<?> optionsSupplier) {
+        super(name, score, optionsSupplier);
     }
 
     public NshBuiltinDefault(String name, Class<?> optionsSupplier) {
         super(name, optionsSupplier);
     }
 
-    public NshBuiltinDefault(String name, int supportLevel, Supplier<?> optionsSupplier) {
-        super(name, supportLevel, optionsSupplier);
+    public NshBuiltinDefault(String name, int score, Supplier<?> optionsSupplier) {
+        super(name, score, optionsSupplier);
     }
 
     public NshBuiltinDefault(String name, Supplier<?> optionsSupplier) {
