88de52f8df93ab4510101bb31d4f37f6b659f097
thevpc <taha.bensalah@gmail.com>
2025-12-21 23:20:08 +0100

    Update WhoamiCommand.java : faster-bean-factory

--- nsh-lib/src/main/java/net/thevpc/nsh/cmd/impl/posix/WhoamiCommand.java
+++ nsh-lib/src/main/java/net/thevpc/nsh/cmd/impl/posix/WhoamiCommand.java
@@ -43,6 +43,8 @@ import net.thevpc.nsh.cmd.NshBuiltinDefault;
 import net.thevpc.nsh.eval.NshExecutionContext;
 import net.thevpc.nuts.util.NBlankable;
 import net.thevpc.nuts.text.NMsg;
+import net.thevpc.nuts.util.NScore;
+import net.thevpc.nuts.util.NScorable;
 
 import java.util.*;
 
@@ -50,10 +52,11 @@ import java.util.*;
  * Created by vpc on 1/7/17.
  */
 @NComponentScope(NScopeType.WORKSPACE)
+@NScore(fixed = NScorable.DEFAULT_SCORE)
 public class WhoamiCommand extends NshBuiltinDefault {
 
     public WhoamiCommand() {
-        super("whoami", DEFAULT_SCORE, Options.class);
+        super("whoami", Options.class);
     }
 
     @Override
