9b7d6da1d4793a64af8ab16882980305a13d2829
thevpc <taha.bensalah@gmail.com>
2025-12-21 23:20:08 +0100

    Update ChmodCommand.java : faster-bean-factory

--- nsh-lib/src/main/java/net/thevpc/nsh/cmd/impl/posix/ChmodCommand.java
+++ nsh-lib/src/main/java/net/thevpc/nsh/cmd/impl/posix/ChmodCommand.java
@@ -35,6 +35,8 @@ import net.thevpc.nuts.spi.NScopeType;
 import net.thevpc.nsh.cmd.NshBuiltinDefault;
 import net.thevpc.nsh.eval.NshExecutionContext;
 import net.thevpc.nuts.text.NMsg;
+import net.thevpc.nuts.util.NScore;
+import net.thevpc.nuts.util.NScorable;
 
 import java.util.*;
 
@@ -42,10 +44,11 @@ import java.util.*;
  * Created by vpc on 1/7/17.
  */
 @NComponentScope(NScopeType.WORKSPACE)
+@NScore(fixed = NScorable.DEFAULT_SCORE)
 public class ChmodCommand extends NshBuiltinDefault {
 
     public ChmodCommand() {
-        super("chmod", DEFAULT_SCORE,Options.class);
+        super("chmod", Options.class);
     }
 
 
