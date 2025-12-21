9cd18b12f9e97f88acc68270201c6fc55dbb2275
thevpc <taha.bensalah@gmail.com>
2025-12-21 23:20:08 +0100

    Update NshBuiltinDefault.java : faster-bean-factory

--- nsh-lib/src/main/java/net/thevpc/nsh/cmd/NshBuiltinDefault.java
+++ nsh-lib/src/main/java/net/thevpc/nsh/cmd/NshBuiltinDefault.java
@@ -3,19 +3,12 @@ package net.thevpc.nsh.cmd;
 import java.util.function.Supplier;
 
 public abstract class NshBuiltinDefault extends NshBuiltinBase {
-    public NshBuiltinDefault(String name, int score, Class<?> optionsSupplier) {
-        super(name, score, optionsSupplier);
-    }
-
     public NshBuiltinDefault(String name, Class<?> optionsSupplier) {
         super(name, optionsSupplier);
-    }
-
-    public NshBuiltinDefault(String name, int score, Supplier<?> optionsSupplier) {
-        super(name, score, optionsSupplier);
     }
 
     public NshBuiltinDefault(String name, Supplier<?> optionsSupplier) {
         super(name, optionsSupplier);
     }
+
 }
